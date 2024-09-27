ec1f6b9a5831440891de00555f4b1dd577ab7876
api-rbtest <hmhieu1412@gmail.com>
2024-09-27 11:38:36 +0700

    update

--- cikm20-wps/src/main/r/install.r
+++ cikm20-wps/src/main/r/install.r
@@ -1,19 +1,27 @@
-packages <- c("jsonlite", "optparse", "png", "raster", "sf", "sp")
+# packages <- c("jsonlite", "optparse", "png", "raster", "sf", "sp")
 
-# Specify the versions for each package
-package_versions <- c("jsonlite" = "1.7.2",
-                      "optparse" = "1.6.6",
-                      "png" = "0.1.7",
-                      "raster" = "3.4.5",
-                      "sf" = "0.9.6",
-                      "sp" = "1.4.4")
+# # Specify the versions for each package
+# package_versions <- c("jsonlite" = "1.7.2",
+#                       "optparse" = "1.6.6",
+#                       "png" = "0.1.7",
+#                       "raster" = "3.4.5",
+#                       "sf" = "0.9.6",
+#                       "sp" = "1.4.4")
 
-# Install packages with specific versions
-for (pkg in packages) {
-  if (!requireNamespace(pkg, quietly = TRUE) || packageVersion(pkg) != package_versions[pkg]) {
-    install.packages(pkg, version = package_versions[pkg], repos = "http://cran.rstudio.com/")
-  }
-}
+# # Install packages with specific versions
+# for (pkg in packages) {
+#   if (!requireNamespace(pkg, quietly = TRUE) || packageVersion(pkg) != package_versions[pkg]) {
+#     print(paste("Installing", pkg, "version", package_versions[pkg]))
+#     install.packages(pkg, version = package_versions[pkg], repos = "http://cran.rstudio.com/")
+#   }
+# }
 
-# Load the packages to confirm installation
-lapply(packages, library, character.only = TRUE)
+# # Load the packages to confirm installation
+# lapply(packages, library, character.only = TRUE)
+
+install.packages('raster', dependencies=TRUE, repos='https://CRAN.R-project.org/')
+install.packages('sf', dependencies=TRUE, repos='https://CRAN.R-project.org/')
+install.packages('sp', dependencies=TRUE, repos='https://CRAN.R-project.org/')
+install.packages('jsonlite', dependencies=TRUE, repos='https://CRAN.R-project.org/')
+install.packages('optparse', dependencies=TRUE, repos='https://CRAN.R-project.org/')
+install.packages('png', dependencies=TRUE, repos='https://CRAN.R-project.org/')
